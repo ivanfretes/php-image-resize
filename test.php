bba9fd7c05fc66ac76474a19d758a1c3fae14312
Ivan Fretes <vanfretes@gmail.com>
2017-02-09 12:21:15 -0300

    Test

---
+++ test.php
@@ -0,0 +1,9 @@
+<?
+	require "Resizeimg.php"; 
+
+
+	$img = new PHPResizeImg\ResizeImg("http://www.bmw.com.mx/content/dam/bmw/common/all-models/m-series/m4-coupe/2014/m4-gts/m4-gts-driving-large-teaser-.jpg/jcr:content/renditions/cq5dam.resized.img.1680.large.time1447949538576.jpg", array(225,125));
+	//$img->setPathImg("img/hola");
+	$img->assignName("nameTest");
+	$img->resizeImg();
+?>
