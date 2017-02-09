62d033c66f4182b8739a5172bd74a172fc413586
Ivan Fretes <vanfretes@gmail.com>
2017-02-09 15:18:46 -0300

    Test car models

--- test.php
+++ test.php
@@ -11,23 +11,22 @@
 							  'C' => 'http://www.mbzlongbeach.com/photo/izmo_tlc/sscusa/2016/16mercedes/16mercedescclssac3004t/mercedes_16cclssac3004t_angularfront_polarwhite.png' ));
 
 	
-	/*foreach ($listMarcas as $marca => $listModelos) {
+	foreach ($listMarcas as $marca => $listModelos) {
 		foreach ($listModelos as $modelo => $url) {
 			$img = new PHPResizeImg\ResizeImg($url, array(225,125));
-			$img->setPathImg("img/hola");
+			//$img->setPathImg("img/hola");
 
 			$img->assignName($marca."_".$modelo);
-			$img->resizeImg();?>
-			<img src="<? //$img->getPathImg()."/".;?>">
-		<? }
-	}*/
-	$url = 'http://images.webmakerx.net/Sites/Site20551/Picture/2011/April/Z4.PNG';
-	$img = new PHPResizeImg\ResizeImg($url, .6);
+			$img->resizeImg();
+		}
+	}
+	/*$url = 'http://images.webmakerx.net/Sites/Site20551/Picture/2011/April/Z4.PNG';
+	$img = new PHPResizeImg\ResizeImg($url, array(225,125));
 	//$img->setPathImg("img/hola");
 
 	//$img->assignName("nameText");
-	$img->resizeImg();
-	//echo "Listo";
+	$img->resizeImg();*/
+	echo "Listo";
 
 ?>
 
